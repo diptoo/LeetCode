50721343f7e4fce0cfbf7d16b7f13fe319bfba92
dipto <dipto33120@gmail.com>
2020-09-21 01:31:50 +0600

    leetcode 118 modified

--- Leetcode118.cpp
+++ Leetcode118.cpp
@@ -1,36 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> result;
+        vector<vector<int>> result(numRows);
         for(int i = 0; i < numRows; i++)
         {
-            vector<int>inres;
-            if(i == 0) 
-            {
-                inres.push_back(1);
-                result.push_back(inres);
-            }
-            else if(i == 1)
-            {
-                inres.push_back(1);
-                inres.push_back(1);
-                result.push_back(inres);
-            }
-            else
-            {
-                int loop_till = i - 1;
-                inres.push_back(1);
-                for(int j = 0; j< loop_till; j++)
-                {
-                    int fi = result[i - 1][j];
-                    int sc = result[i - 1][j + 1];
-                    //cout<<fi<<" "<<sc<<endl;
-                    inres.push_back(fi + sc);
-                }
-                inres.push_back(1);
-                result.push_back(inres);
-            }
+            result[i].resize(i + 1);
             
+            result[i][0] = 1;
+            result[i][i] = 1;
+
+            int loop_till = i - 1;
+            result[i][0] = 1;
+            for(int j = 0; j< loop_till; j++)
+            {
+                int fi = result[i - 1][j];
+                int sc = result[i - 1][j + 1];
+                //cout<<fi<<" "<<sc<<endl;
+                result[i][j+1] = fi + sc;
+            }            
         }
         
         return result;
